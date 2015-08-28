92ae74e33267faf4120ad491b7ecc0ec0ac82f05
Jordan <jordanjohan@gmail.com>
2015-08-28 13:27:20 -0700

    Create routes for links from homepage.

--- app/app.php
+++ app/app.php
@@ -17,10 +17,23 @@
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views/'));
 
+    //Home page
     $app->get('/', function() use ($app) {
 
         return $app['twig']->render('index.html.twig', array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
     });
 
+    /* Linked from home page "View list of stores."  Links to separate page with store list. */
+    $app->get('/stores', function() use ($app) {
 
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
+
+    /* Linked from home page "Browse by brand" link.  Takes user to separeate page dispalying list of all added brands. */
+    $app->get('/brands', function() use ($app) {
+
+        return $app['twig']->render('brands.html.twig', array('stores' => Brand::getAll()));
+    });
+
+    return $app;
 ?>

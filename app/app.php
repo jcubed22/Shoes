8b1fec81c1cf2f3fa5ab9a0a12fc0aa158258171
Jordan <jordanjohan@gmail.com>
2015-08-28 15:03:08 -0700

    Add route for to individual brand page.

--- app/app.php
+++ app/app.php
@@ -33,9 +33,11 @@
     $app->post('/stores', function() use ($app) {
         $store = new Store($_POST['retailer'], $_POST['address'], $_POST['phone']);
         $store->save();
+
         return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
     });
 
+    /* Route for deleting all stores from Stores page */
     $app->post('/delete_stores', function() use ($app) {
         Store::deleteAll();
 
@@ -48,16 +50,26 @@
         return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
     });
 
+    /* Route for creating a new brand from the brands pgae */
     $app->post('/brands', function() use ($app) {
         $brand = new Brand($_POST['name']);
         $brand->save();
+
         return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
     });
 
+    /* Route for deleting all brands from the brand page */
     $app->post('/delete_brands', function() use ($app) {
         Brand::deleteAll();
 
         return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+    /* Route from brands page brand name link to view individual brand and list of stores for that brand */
+    $app->get('/brands/{id}', function ($id) use ($app) {
+        $brand = Brand::find($id);
+
+        return $app['twig']->render('brand.html.twig', array('brand' => $brand, 'stores' => $brand->getStores(), 'all_stores' => Store::getAll()));
     });
 
     return $app;
